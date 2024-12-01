2ffe5c43acdc4d5e490589be110dd4d9dfaca393
mahruf60 <mahruf9060@gmail.com>
2024-12-01 14:17:35 +0600

    Introduction to JavaScript

---
+++ class-1/index.js
@@ -0,0 +1,8 @@
+// Introduction class in JavaScript 
+console.log("This program is adds three numbers.")
+
+function addThreeNumbers(a,b,c){
+    return a + b + c;
+}
+let add = addThreeNumbers(10,20,30)
+console.log(add)
