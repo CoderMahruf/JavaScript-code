7b957ee4b3365695a1e7135bcf56c67d0a077cc0
mahruf60 <mahruf9060@gmail.com>
2024-12-18 23:45:31 +0600

    JavaScript Array Methods

---
+++ class-17/index.js
@@ -0,0 +1,32 @@
+// JavaScript Array Methods
+// toString()
+let fruits = ["Apple", "Orange", "Banana"];
+console.log(fruits.toString()); 
+console.log(typeof fruits.toString()); 
+// join()
+let fruits2 = ["Apple", "Orange", "Banana"];
+console.log(fruits2.join("-")); 
+console.log(typeof fruits2.join("-")); 
+
+let fruits3 = ["Apple", "Orange", "Banana"];
+console.log(fruits3.join(" and "));
+
+// pop()
+let fruits4 = ["Apple", "Orange", "Banana"];
+console.log(fruits4.pop()); 
+console.log(fruits4);
+
+// push()
+let fruits5 = ["Apple", "Orange", "Banana"];
+console.log(fruits5.push("Pineapple"));
+console.log(fruits5); 
+
+// shift()
+let fruits6 = ["Apple", "Orange", "Banana"];
+console.log(fruits6.shift()); 
+console.log(fruits6); 
+
+// unshift()
+let fruits7 = ["Apple", "Orange", "Banana"];
+console.log(fruits7.unshift("Pineapple"));
+console.log(fruits7); 
